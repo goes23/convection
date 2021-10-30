d42bc283ccfd0d1a67b5f19ae699c124ab35a5c0
Taufik agus setiana <agoessetiana@gmail.com>
2021-10-30 20:32:14 +0700

    update

--- app/Exports/UtangExport.php
+++ app/Exports/UtangExport.php
@@ -24,15 +24,45 @@ class UtangExport implements FromCollection, WithHeadings
 
     public function collection()
     {
+    //     $query = DB::table('hutang');
+    //     $query->select(
+    //         'name',
+    //         'jumlah_hutang',
+    //         DB::Raw('DATE(tanggal_hutang)')
+    //     );
+
+    //     if ($this->param['kategori'] == 'date')
+    //         $query->whereBetween('tanggal_hutang', [$this->param['start'], $this->param['end']]);
+
+    //     $result = $query->get();
+    //     return $result;
+    // }
+    // public function headings(): array
+    // {
+    //     return [
+    //         "NAMA", "JUMLAH HUTANG", "TANGGAL HUTANG"
+    //     ];
+    // }
+
+    // public function collection()
+    // {
         $query = DB::table('hutang');
+        $query->leftJoin('log_hutang', 'log_hutang.hutang_id', '=', 'hutang.id');
         $query->select(
-            'name',
-            'jumlah_hutang',
-            DB::Raw('DATE(tanggal_hutang)')
+            'hutang.jumlah_hutang',
+            'hutang.sisa',
+            DB::Raw('DATE(hutang.tanggal_hutang)'),
+            'log_hutang.jumlah_pembayaran',
+            DB::Raw('DATE(log_hutang.tanggal_pembayaran)'),
+            'log_hutang.keterangan'
         );
+        // $query->selectRaw('SELECT name FROM product WHERE product.id = produksi.product_id');
 
         if ($this->param['kategori'] == 'date')
-            $query->whereBetween('tanggal_hutang', [$this->param['start'], $this->param['end']]);
+            $query->whereBetween('hutang.tanggal_hutang', [$this->param['start'], $this->param['end']]);
+
+        if ($this->param['kategori'] == 'date')
+            $query->whereBetween('log_hutang.tanggal_pembayaran', [$this->param['start'], $this->param['end']]);
 
         $result = $query->get();
         return $result;
@@ -40,7 +70,7 @@ class UtangExport implements FromCollection, WithHeadings
     public function headings(): array
     {
         return [
-            "NAMA", "JUMLAH HUTANG", "TANGGAL HUTANG"
+             "TOTAL HUTANG", "SISA HUTANG", "TANGGAL TRANSAKSI", "JUMLAH PEMBAYARAN", "TANGGAL PEMBAYARAN", "KETERANGAN"
         ];
     }
 }
